c0d07daf5b545853335d983cd885ed1995e5bc11
Ashenafi-Tesfaye <rashenafibab@gmail.com>
2021-11-20 15:26:09 -0500

    initial commit

---
+++ AGPayPalSpringdemo/src/main/java/com/AGPayPalExample/AGPayPalSpringdemo/AgPayPalSpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.AGPayPalExample.AGPayPalSpringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgPayPalSpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgPayPalSpringdemoApplication.class, args);
+	}
+
+}
---
+++ AGPayPalSpringdemo/src/test/java/com/AGPayPalExample/AGPayPalSpringdemo/AgPayPalSpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AGPayPalExample.AGPayPalSpringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgPayPalSpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
